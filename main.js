606984d081b4ff34b7fb3c4cbba36c5d8dd969e0
kinragiez <alexzeigarnik@outlook.com>
2025-03-11 22:55:24 +0200

    Add project configuration files and update main.js with a greeting function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("bubbles");
+/*
+Author: Alex Zeigarnik (kinragiez)
+Email: alexzeigarnik@outlook.com
+Version: 0.0
+*/
+
+function greet(name) {
+    return `Hello, ${name}! Welcome!`;
+}
+console.log(greet('User'));
